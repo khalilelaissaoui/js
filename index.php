2b4cd259e42209d4d63578c8970ea9dfa205caf3
khalilelaissaoui <khalilelaissaoui92@gmail.com>
2025-12-23 19:59:26 +0100

    index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title> Mohammed Khalil El aissaoui 8809e9b5</title>
+    <title>Mohammed Khalil Elaissaoui 8809e9b5</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
         integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
@@ -16,7 +16,7 @@
 ?>
 
 <body>
-    <h1>Mohammed Khalil El aissaoui</h1>
+    <h1>Mohammed Khalil Elaissaoui</h1>
 
     <?php
         if (isset($_SESSION['success'])) {
